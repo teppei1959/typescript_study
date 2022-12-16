b3e562bf5d06b0b2345512136eb69e4b3962a1e4
teppeiUeno <t-ueno@adventure-inc.co.jp>
2022-12-16 14:46:20 +0900

    関数テスト

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,20 @@
 // worldTest.sayHello(root)
 
 // 基本の型定義
-import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
+// import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
 
-primitiveSample()
-notExistSample()
-anySample()
-unknownSample()
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unknownSample()
+
+// 関数の型定義
+import { logMessage } from "./function/basic"
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice} from "./function/parameters"
+
+logMessage("Hello Typescript!")
+isUserSignedIn("ABC", "トラハック")
+isUserSignedIn("DEF")
+isUserSignedIn2("ABC")
+const sum = sumProductsPrice(100,200,300,400,500)
+console.log('Function parameters sample 5:', sum)
